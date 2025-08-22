9e16fc862d4575bc7d7801f295283fb33d2de8d3
HypeNeeraj <159238803+HypeNeeraj@users.noreply.github.com>
2025-08-22 05:14:41 -0700

    Add files via upload
    Added task1 and task2

---
+++ Task1.py
@@ -0,0 +1,10 @@
+a=int(input("Enter the first Number: "))
+b=int(input("Enter the second Number: "))
+sum = a + b
+sub = a - b
+multi = a * b
+div = a / b
+print ("Addition:",sum)
+print ("Subtraction:",sub)
+print ("Multiplication:",multi)
+print ("Division:",div)
---
+++ Task2.py
@@ -0,0 +1,6 @@
+
+a = str(input("Enter the First name: "))
+b = str(input("Enter the Last name: "))
+full_name = a + " " + b
+
+print(f"Hello, {full_name}! Welcome to the python program.")
